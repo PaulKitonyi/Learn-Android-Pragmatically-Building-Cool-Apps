ca5813e48e5b68b1131db13f289baa3c556c22c2
Paul Kitonyi <paulmucimah@gmail.com>
2020-04-09 11:14:36 +0300

    removed some bugs

--- showName/app/src/main/java/com/paulkitonyi/showname/MainActivity.java
+++ showName/app/src/main/java/com/paulkitonyi/showname/MainActivity.java
@@ -1,8 +1,10 @@
--package com.paulkitonyi.showname;
+package com.paulkitonyi.showname;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.paulkitonyi.showname.R;
 
 public class MainActivity extends AppCompatActivity {
 
