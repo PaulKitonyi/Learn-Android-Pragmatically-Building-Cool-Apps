0104b35550d5f58e658230fd37ea8f77c554db54
Paul Kitonyi <paulmucimah@gmail.com>
2020-04-09 12:45:10 +0300

    final showName project

--- showName/app/src/main/java/com/paulkitonyi/showname/MainActivity.java
+++ showName/app/src/main/java/com/paulkitonyi/showname/MainActivity.java
@@ -3,14 +3,40 @@ package com.paulkitonyi.showname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.paulkitonyi.showname.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mybtn;
+    private TextView myTxtView;
+    private EditText enterName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mybtn = findViewById(R.id.button);
+        myTxtView = findViewById(R.id.textView);
+        enterName = findViewById(R.id.editText);
+
+        enterName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterName.setText("");
+            }
+        });
+
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = enterName.getText().toString();
+                myTxtView.setText("Your name is "+name);
+                enterName.setText("Enter a another name!!");
+            }
+        });
     }
 }
